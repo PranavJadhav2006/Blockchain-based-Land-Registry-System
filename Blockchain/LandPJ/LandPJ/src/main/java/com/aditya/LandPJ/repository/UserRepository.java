f9653eab186c12dca553e5f14e41ae74649727a7
PranavJadhav2006 <pranavjadhav1319@gmail.com>
2025-06-07 01:01:32 +0530

    Delete Blockchain/LandPJ/LandPJ directory

--- Blockchain/LandPJ/LandPJ/src/main/java/com/aditya/LandPJ/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aditya.LandPJ.repository;
-
-import com.aditya.LandPJ.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
