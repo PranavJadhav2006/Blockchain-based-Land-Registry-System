d703f68747813e6ec79e618875bb5713f735b084
PranavJadhav2006 <pranavjadhav1319@gmail.com>
2025-06-07 01:03:11 +0530

    Add files via upload

---
+++ Blockchain-based-Land-Registry-System-main/Blockchain-based-Land-Registry-System-main/Blockchain/LandPJ/LandPJ/src/test/java/com/aditya/LandPJ/LandPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.LandPJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
